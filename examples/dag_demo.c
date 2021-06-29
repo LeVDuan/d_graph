8e9b4157a6a5301e13b6d7e064e17450c123645d
LE VAN DUAN <duan.lv0308@outlook.com>
2021-06-29 15:40:42 +0700

    update dag_demo

--- examples/dag_demo.c
+++ examples/dag_demo.c
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
 {
 	printf("dag examples\n");
 
-	d_graph_t g = d_graph_create(D_GRAPH_UNDIRECTED);
+	d_graph_t g = d_graph_create(D_GRAPH_DIRECTED);
 
 	d_graph_add_Vertice(g, 1, "v1");
 	d_graph_add_Vertice(g, 2, "v2");
@@ -14,10 +14,13 @@ int main(int argc, char const *argv[])
 	d_graph_add_Edge(g, 1, 2);
 	d_graph_add_Edge(g, 2, 3);
 	d_graph_add_Edge(g, 3, 4);
-	d_graph_add_Edge(g, 1, 4);
+	d_graph_add_Edge(g, 4, 1);
 
 	if(d_graph_Is_DAG(g)){
 		printf("is dag\n");
+	}
+	else{
+		printf("is not dag\n");
 	}
 	d_graph_drop(&g);
 	return 0;
